4138c76e5185ec28af214e32de5e9424e7ef82c5
Dillon Timmer <dtimmer707@gmail.com>
2021-05-31 23:36:05 -0700

    redirect handling

--- client.go
+++ client.go
@@ -1,24 +1,52 @@
 package cclient
 
 import (
-	"golang.org/x/net/proxy"
 	"net/http"
+
+	"golang.org/x/net/proxy"
 
 	utls "github.com/refraction-networking/utls"
 )
 
-func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
-	if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
-		dialer, err := newConnectDialer(proxyUrl[0])
-		if err != nil {
-			return http.Client{}, err
+func NewClient(clientHello utls.ClientHelloID, redirectOption string, proxyUrl ...string) (http.Client, error) {
+
+	if redirectOption == "False" {
+		if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
+			dialer, err := newConnectDialer(proxyUrl[0])
+			if err != nil {
+				return http.Client{
+					CheckRedirect: func(req *http.Request, via []*http.Request) error {
+						return http.ErrUseLastResponse
+					},
+				}, err
+			}
+			return http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+				Transport: newRoundTripper(clientHello, dialer),
+			}, nil
+		} else {
+			return http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+				Transport: newRoundTripper(clientHello, proxy.Direct),
+			}, nil
 		}
-		return http.Client{
-			Transport: newRoundTripper(clientHello, dialer),
-		}, nil
 	} else {
-		return http.Client{
-			Transport: newRoundTripper(clientHello, proxy.Direct),
-		}, nil
+		if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
+			dialer, err := newConnectDialer(proxyUrl[0])
+			if err != nil {
+				return http.Client{}, err
+			}
+			return http.Client{
+				Transport: newRoundTripper(clientHello, dialer),
+			}, nil
+		} else {
+			return http.Client{
+				Transport: newRoundTripper(clientHello, proxy.Direct),
+			}, nil
+		}
 	}
 }
